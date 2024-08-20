a593921f7dd9e895974531a74d11f620710a5aab
Mariano Pistone <livingtheweb01@gmail.com>
2024-08-20 09:45:14 +0200

    nextjs-02 Routing - Dynamic routes using brackets - fix-01

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductList = () => {
   return (
     <div>
-      <h1>Product list</h1>
+      <h1>Product list </h1>
       <h2>Product 1</h2>
       <h2>Product 2</h2>
       <h2>Product 3</h2>
